0f0d6bcd13253a962d09067cc4000cf50ff93027
Jack Phoenix <jack@countervandalism.net>
2014-10-27 01:25:13 +0200

    Version 0.3.0 -- more RequestContext, less legacy i18n
    Change-Id: Iae22af7f3ebce8fd015ff231f50ed78524615521

--- Quantcast.php
+++ Quantcast.php
@@ -5,27 +5,27 @@
  * @file
  * @ingroup Extensions
  * @author Jack Phoenix <jack@shoutwiki.com> (forgive me)
- * @license http://en.wikipedia.org/wiki/Public_domain Public domain
- * @link http://www.mediawiki.org/wiki/Extension:Quantcast Documentation
- * @see http://bugzilla.shoutwiki.com/show_bug.cgi?id=108
+ * @license https://en.wikipedia.org/wiki/Public_domain Public domain
+ * @link https://www.mediawiki.org/wiki/Extension:Quantcast Documentation
+ * @see https://bugzilla.shoutwiki.com/show_bug.cgi?id=108
  */
 
 if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'This is not a valid entry point to MediaWiki.' );
 }
 
-$wgMessagesDirs['QuantCast'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['Quantcast'] =  __DIR__ . '/Quantcast.i18n.php';
-
 // Extension credits that will show up on Special:Version
 $wgExtensionCredits['other'][] = array(
 	'path' => __FILE__,
 	'name' => 'Quantcast Tracking',
-	'version' => '0.2.0',
+	'version' => '0.3.0',
 	'author' => 'Jack Phoenix',
 	'descriptionmsg' => 'quantcast-desc',
 	'url' => 'https://www.mediawiki.org/wiki/Extension:Quantcast',
 );
+
+// i18n
+$wgMessagesDirs['Quantcast'] = __DIR__ . '/i18n';
 
 // Groups that are excluded from Quantcast statistics
 $wgQuantcastTrackingExcludedGroups = array( 'staff' );
@@ -37,21 +37,21 @@ $wgHooks['SkinAfterBottomScripts'][] = 'wfAddQuantcastTrackingCode';
  * Add tracking JS to all pages for all users that are not members of excluded
  * groups (the group listed in $wgQuantcastTrackingExcludedGroups).
  *
- * @param $skin Object: Skin object
- * @param $text String: bottomScripts text
- * @return Boolean: true
+ * @param Skin $skin
+ * @param string $text bottomScripts text
+ * @return bool
  */
 function wfAddQuantcastTrackingCode( $skin, &$text ) {
-	global $wgUser, $wgQuantcastTrackingExcludedGroups;
+	global $wgQuantcastTrackingExcludedGroups;
 
-	$groups = $wgUser->getEffectiveGroups();
+	$groups = $skin->getUser()->getEffectiveGroups();
 	if ( !in_array( $wgQuantcastTrackingExcludedGroups, $groups ) ) {
-		$message = trim( wfMsgForContent( 'quantcast-tracking-number' ) );
+		$message = $skin->msg( 'quantcast-tracking-number' )->inContentLanguage();
 		// We have a custom tracking code, use it!
-		if( !wfEmptyMsg( 'quantcast-tracking-number', $message ) ) {
-			$trackingCode = $message;
+		if ( !$message->isDisabled() ) {
+			$trackingCode = trim( $message->text() );
 		} else { // use ShoutWiki's default code
-			$trackingCode = wfMsgForContent( 'shoutwiki-quantcast-tracking-number' );
+			$trackingCode = $skin->msg( 'shoutwiki-quantcast-tracking-number' )->inContentLanguage()->text();
 		}
 		$safeCode = htmlspecialchars( $trackingCode, ENT_QUOTES );
 		$text .= "\t\t" . '<!-- Start Quantcast tag -->
